3891d9b12d7e9636729f03530d686f5437a76115
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-10-22 09:31:57 +0300

    create test folder

--- config.py
+++ config.py
@@ -2,15 +2,14 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://machel:jkl@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://machel:jkl@localhost/pitchit'
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://machel:jkl@localhost/pitch'
+    pass
 
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://machel:jkl@localhost/pitch'
     DEBUG = True
 
 config_options = {
