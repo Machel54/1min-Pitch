b6b405e6a74314493f092f7b3d3fe7db09895c8d
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-10-22 11:32:04 +0300

    created all categories and to display their pitches

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
 from flask import render_template,redirect,url_for
+from flask_login import login_required
 from . import main
 
 # Views
@@ -12,3 +13,35 @@ def index():
 
     
     return render_template('index.html')
+
+@main.route('/promotion/pitches/')
+def promotion():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Promotion Pitches'
+
+    pitches= Pitch.get_all_pitches()
+
+    return render_template('promotion.html', title = title, pitches= pitches )
+
+@main.route('/product/pitches/')
+def product():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Product Pitches'
+    pitches= Pitch.get_all_pitches()
+    return render_template('product.html', title = title, pitches= pitches )
+
+@main.route('/interview/pitches/')
+def interview():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    pitches= Pitch.get_all_pitches()
+    title = 'Home - Welcome to The best Pitching Website Online'  
+    return render_template('interview.html', title = title, pitches= pitches )
+
+
+
