b1dcf754f65360b11770eae0845f5786ed40bbcf
Machel Nyanumba <machelnyanumba14@gmail.comgit config --global user.email machelnyanumba14@gmail.com>
2019-10-24 05:48:06 +0300

    create update profile pic section on the user login

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 from config import config_options
 
 
@@ -11,6 +13,8 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -22,7 +26,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-
+    mail.init_app(app)
     
     # Registering the blueprint
     from .main import main as main_blueprint
--- app/main/forms.py
+++ app/main/forms.py
@@ -10,4 +10,14 @@ class PitchForm(FlaskForm):
 class CommentsForm(FlaskForm):
     comment = TextAreaField('Comment', validators=[Required()])
     vote=RadioField('default field arguments', choices=[('1', 'UpVote'), ('1', 'DownVote')])
-    submit = SubmitField('SUBMIT')  
+    submit = SubmitField('SUBMIT') 
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')  
+
+class UpvoteForm(FlaskForm):
+    '''
+    Class to create a wtf form for upvoting a pitch
+    '''
+    submit = SubmitField('Upvote')
